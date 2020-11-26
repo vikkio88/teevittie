a832b8cde99460e0b675771978d2fd3460e1c046
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-11-26 21:01:32 +0000

    moved to react, fuck svelte for small projects

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Tivitti</h1>
+      <button>Test</button>
+    </main>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'mini.css/dist/mini-dark.min.css';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
