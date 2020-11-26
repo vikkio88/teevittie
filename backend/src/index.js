129cab54f0ea297861b699ce934166c404939cbb
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-11-26 21:01:18 +0000

    made api group

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,18 @@
 if (process.env.NODE_ENV === 'development') require('dotenv').config();
 const cors = require('micro-cors')();
 const { send } = require('micro');
-const { router, get, post } = require('microrouter');
+const { router, get, post, withNamespace } = require('microrouter');
 
 const { misc, catalog } = require('./actions');
 
+const api = withNamespace('/api');
 
 module.exports = cors(
     router(
-        get('/ping', misc.pong),
-
-        get('/catalog', catalog.all),
-
+        api(
+            get('/ping', misc.pong),
+            get('/catalog', catalog.all),
+        ),
         get('/', misc.fallback),
 
         get('/*', (req, res) => send(res, 404)),
