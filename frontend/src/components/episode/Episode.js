8c05e22c173d64d598004125be306dc0e000f451
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-09-04 09:13:40 +0100

    two ways of updating/logging time

--- frontend/src/components/episode/Episode.js
+++ frontend/src/components/episode/Episode.js
@@ -1,10 +1,24 @@
+import { useEffect } from 'react';
 import './styles/Episode.css';
 const { REACT_APP_API_URL } = process.env;
+// const timeUpdateHandler = ({ target }) => {
+//     console.log('time update:', target.currentTime);
+// };
 
 const Episode = ({ videoId }) => {
+    // useEffect(() => {
+    //     const video = document.getElementById('video');
+    //     video.addEventListener('timeupdate', timeUpdateHandler);
+    //     return () => video.removeEventListener('timeupdate', timeUpdateHandler);
+    // });
+    useEffect(() => {
+        const video = document.getElementById('video');
+        const interval = setInterval(() => console.log(video.currentTime), 5000);
+        return () => clearInterval(interval);
+    });
     return (
         <div className="Episode-wrapper">
-            <video width="100%" controls>
+            <video id="video" width="100%" controls>
                 <source src={`${REACT_APP_API_URL}/stream/${videoId}`} type="video/mp4" />
             </video>
 
