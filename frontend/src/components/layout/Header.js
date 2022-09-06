ce45f75cab79968ab47ea7accea73b876978eaa2
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-09-06 20:19:20 +0100

    fixed logo issue

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
 import './styles/Header.css';
 
+import gh from 'assets/gh.svg';
+
 const Header = () => {
     return (
         <header>
             <Link to='/'><h1 className="logo" >📺-ttie</h1></Link>
             <div className="credits">
                 <a href="https://github.com/vikkio88/teevittie" target="_blank">
-                    <img width="50px" src="assets/gh.svg" alt="Github" />
+                    <img width="50px" src={gh} alt="Github" />
                 </a>
             </div>
         </header>
