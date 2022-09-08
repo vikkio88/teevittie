aac80a4259b22d8f4070fc113d92bc624ba3a9d5
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-09-08 18:50:04 +0100

    moved version to header

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -3,12 +3,13 @@ import './styles/Header.css';
 
 import gh from 'assets/gh.svg';
 
-const Header = () => {
+const Header = ({ version }) => {
     return (
         <header>
             <Link to='/'><h1 className="logo" >📺-ttie</h1></Link>
+            <small>{version}</small>
             <div className="credits">
-                <a href="https://github.com/vikkio88/teevittie" target="_blank">
+                <a href="https://github.com/vikkio88/teevittie" target="_blank" rel="noreferrer">
                     <img width="50px" src={gh} alt="Github" />
                 </a>
             </div>
