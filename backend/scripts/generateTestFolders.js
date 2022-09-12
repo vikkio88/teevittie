a427bce3f1d9f6afe9d70fb81baf134017c7a1a0
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-09-12 19:38:41 +0100

    fixed small bug

--- backend/scripts/generateTestFolders.js
+++ backend/scripts/generateTestFolders.js
@@ -3,26 +3,26 @@ const path = require('path');
 const randomString = () => Math.random().toString(36).substring(7);
 const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-const dir = process.argv.slice(2) || './tmp';
+const dir = process.argv.slice(2)[0] || './tmp';
 
 if (fs.existsSync(dir)) {
-    fs.rmdirSync(dir, { recursive: true });
+    fs.rmSync(dir, { recursive: true });
 }
 
 fs.mkdirSync(dir);
-const shows = randomInt(2, 5);
+const shows = randomInt(3, 8);
 console.log(`Generating ${shows} shows`);
 for (let i = 0; i < shows; i++) {
     const show = `Show ${randomString()}`;
     console.log(`  generating show: ${show}`);
     fs.mkdirSync(path.join(dir, show));
-    const seasons = randomInt(1, 4);
+    const seasons = randomInt(1, 7);
     console.log(`       generating ${seasons} seasons`);
     for (let j = 0; j < seasons; j++) {
         const season = `Season ${j + 1}`;
         console.log(`       generating season ${season}`);
         fs.mkdirSync(path.join(dir, show, season));
-        const episodes = randomInt(1, 4);
+        const episodes = randomInt(2, 15);
         console.log(`       generating ${episodes} episodes`);
         for (let k = 0; k < episodes; k++) {
             const episode = `Episode ${randomString()} s0${j + 1}e${k + 1}.mp4`;
