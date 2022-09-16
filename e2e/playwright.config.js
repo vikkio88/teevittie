f596f8cbf6cb72716c9c0361855c8d8908453e71
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-09-16 08:01:00 +0100

    moved e2e tests to subpackage

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -13,7 +13,7 @@ const { devices } = require('@playwright/test');
  * @type {import('@playwright/test').PlaywrightTestConfig}
  */
 const config = {
-  testDir: './e2e',
+  testDir: './tests',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
--- e2e/tests/simple_journey.spec.js
+++ e2e/tests/simple_journey.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
+test('homepage to show journey', async ({ page }) => {
   await page.goto('http://localhost:3001/');
 
   await expect(page).toHaveTitle(/Teevittie/);
