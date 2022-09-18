9635615aabeb29681d1808c51ecf49484ce174e9
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-09-18 09:59:20 +0100

    adding subs generation

--- backend/scripts/generateTestFolders.js
+++ backend/scripts/generateTestFolders.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const randomString = () => Math.random().toString(36).substring(7);
 const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const randomBool = (perc = 50) => randomInt(0, 100) >= perc;
 
 const dir = process.argv.slice(2)[0] || './tmp';
 
@@ -25,11 +26,19 @@ for (let i = 0; i < shows; i++) {
         const episodes = randomInt(2, 15);
         console.log(`       generating ${episodes} episodes`);
         for (let k = 0; k < episodes; k++) {
-            const episode = `Episode ${randomString()} s0${j + 1}e${k + 1}.mp4`;
+            const episodeName = `Episode ${randomString()} s0${j + 1}e${k + 1}`;
+            const episode = `${episodeName}.mp4`;
             console.log(`       generating episode: ${episode}`);
-            const filepath = path.join(dir, show, season, episode);
-            console.log('FILE:', filepath);
-            fs.copyFileSync('scripts/example.mp4', filepath);
+            const episodeFilepath = path.join(dir, show, season, episode);
+            console.log('FILE:', episodeFilepath);
+            fs.copyFileSync('scripts/example.mp4', episodeFilepath);
+            if (randomBool()) {
+                console.log(`       -- adding subtitle to episode: ${episode}`);
+                const subtitleFilePath = path.join(dir, show, season, `${episodeName}.vvt`);
+                fs.copyFileSync('scripts/example.vvt', subtitleFilePath);
+            }
+
+
         }
     }
 }
