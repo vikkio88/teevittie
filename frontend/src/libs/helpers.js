b17d2b7e8122f96bea0c85c69ca4f61167589836
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-09-27 10:55:15 +0100

    implemented stray filtering

--- frontend/src/libs/helpers.js
+++ frontend/src/libs/helpers.js
@@ -5,11 +5,12 @@ const catalogHelper = {
         if (ids.length === 3) {
             return catalog.find(s => s.id === ids[0])?.seasons?.find(s => s.id === ids[1])?.episodes.find(e => e.id === ids[2]) ?? null;
         }
-
-        // HERE I NEED TO ADD A WAY TO GET THE OTHERs FOLDER
         
+        // check whether last one has a `meta: { stray: true }`        
+        const strayFolder = catalog.find(f => f?.meta?.stray ?? false);
+        if (!strayFolder) return null;
 
-        return null;
+        return strayFolder?.movies?.find(m => m.fullId === episodeId) ?? null;
     },
     getShowById(showId, catalog = []) {
         return catalog.find(s => s.id === showId);
--- frontend/src/libs/helpers.test.js
+++ frontend/src/libs/helpers.test.js
@@ -16,6 +16,14 @@ const mockCatalog = [
                 ]
             }
         ]
+    },
+    {
+        id: 'strayMoviesFolder',
+        meta: { stray: true },
+        name: 'someStuff',
+        movies: [
+            { fullId: 'strayMoviesFolder.movie1' }
+        ]
     }
 ];
 
@@ -30,9 +38,19 @@ describe("Catalog Helper tests", () => {
         const result = catalogHelper.getEpisodeById('show1.episode1', mockCatalog);
         expect(result).toBe(null);
     });
-    
+
     it("Get Episode By Id returns null if catalog empty", () => {
         const result = catalogHelper.getEpisodeById('show1.season1.episode1', []);
+        expect(result).toBe(null);
+    });
+
+    it("Get movie By Id returns a correct entry from the stray movie catalog entry", () => {
+        const result = catalogHelper.getEpisodeById('strayMoviesFolder.movie1', mockCatalog);
+        expect(result).toEqual({ fullId: 'strayMoviesFolder.movie1' });
+    });
+
+    it("Get movie By Id returns null from the stray movie catalog entry if not a valid id", () => {
+        const result = catalogHelper.getEpisodeById('strayMoviesFolder.movie2', mockCatalog);
         expect(result).toBe(null);
     });
 });
